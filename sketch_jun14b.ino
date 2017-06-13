7d3c3aa3d24a61119cf0a224babde660141e6965
dianapascu <pascu.diana94@gmail.com>
2017-06-14 02:01:27 +0300

    Add files via upload

---
+++ sketch_jun14b.ino
@@ -0,0 +1,32 @@
+int incomingByte;   // for incoming serial data
+char dateAndHour[23];
+int i = 0;
+
+void setup() {
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  setAlarm();
+  //delay(1000);
+}
+
+void setAlarm(){
+
+
+        // send data only when you receive data:
+        while (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+
+                dateAndHour[i] = char(incomingByte);
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(dateAndHour[i]);
+
+                i++;
+        }
+  
+}
+ 
+
